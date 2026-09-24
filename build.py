1b28ea31af72535cab3226fe4274dffc536b57c7
Ivan Klimenko <ivan.klimenko@gmail.com>
2026-09-24 11:56:06 +0300

    Реестр: галочка «Валюта отдельной колонкой» для сравнения вариантов

--- build.py
+++ build.py
@@ -1,13 +1,14 @@
-"""Собирает grid.html из grid.tpl.html, подкладывая style / svg / script из index.html (единый код drawer)."""
-import re,os
+"""Собирает grid.html и rub-grid.html из шаблонов, подкладывая style / svg / script из index.html (единый код drawer)."""
+import os
 d=os.path.dirname(os.path.abspath(__file__))
 idx=open(os.path.join(d,'index.html'),encoding='utf-8').read()
-tpl=open(os.path.join(d,'grid.tpl.html'),encoding='utf-8').read()
 def cut(s,a,b):
     i=s.index(a); j=s.index(b,i)+len(b); return s[i:j]
 style=cut(idx,'<style>','</style>')
 svg=cut(idx,'<svg width="0"','</svg>')
 script=idx[idx.index('<script>'):idx.rindex('</script>')+len('</script>')]
-out=tpl.replace('{{STYLE}}',style).replace('{{SVG}}',svg).replace('{{SCRIPT}}',script)
-open(os.path.join(d,'grid.html'),'w',encoding='utf-8').write(out)
-print('grid.html',len(out))
+for name in ('grid','rub-grid'):
+    tpl=open(os.path.join(d,name+'.tpl.html'),encoding='utf-8').read()
+    out=tpl.replace('{{STYLE}}',style).replace('{{SVG}}',svg).replace('{{SCRIPT}}',script)
+    open(os.path.join(d,name+'.html'),'w',encoding='utf-8').write(out)
+    print(name+'.html',len(out))
